3b741a4542cb2b7c34f079ed8c707875367cc5cd
“Glecio <glecio.santos@gmail.com>
2023-10-15 13:47:52 -0300

    tailwind config and packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens:{
+      sm: '375px', 
+      md: '768px',
+      lg: '1440px'
+    },
+    extend: {
+      colors:{
+        eucaplyptus: '#44FFA1',
+        darkpurple: '#24053E',
+        davysgrey: '#584D62',
+        ghostwhite: '#FCF8FF'
+      },
+      fontFamily:{
+        sans: ['Manrope', 'sans'],
+        heading: [ 'Fraunces', 'sans-serif' ]
+      }
+    },
   },
   plugins: [],
 }
